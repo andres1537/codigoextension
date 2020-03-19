369a777499787efb48ec3db5fb6a96201a9f9738
Carlos A. Gómez <andres1537@gmail.com>
2020-03-19 16:20:36 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,29 +2,28 @@ var data = 'https://docs.google.com/spreadsheets/d/1FP9WUxjOEljzcL4-Uhvwk2U20bJ4
 
 function search() {
 	var extension = $('#extension').val();
+	
 	if (extension == '') {
 		alert('Ingrese un número de extensión');
+		$('#result').text('');
 		return true;
 	} else if (isNaN(extension)) {
 		alert('Ingrese un número de extensión válido');
+		$('#result').text('');
 		return true;
 	}
-	
-	// Compile Handlebars template for team RBI leaders.
-	var RBITemplate = Handlebars.compile($('#team-rbi-template').html());
 	  
-	$('#team-rbi').sheetrock({
+	$('#contrasena-ul').sheetrock({
 		reset: true,
 		url: data,
 		sql: "select A, B where A = " + extension,
-		labels: ['extension', 'codigo'],
-		rowHandler: RBITemplate,
 		callback: function (error, options, response) {
 			if (response.rows.length <= 1) {
-				alert('No se encontró ningún código');	
+				alert('No se encontró ninguna contraseña');
+				$('#result').text('');
+			} else {
+				$('#result').text(response.rows[1].cellsArray[1]);
 			}
-			
-			$('#extension').val('');
 		}
 	});
 	return true;
